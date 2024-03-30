4bff84a74f942d39f4e5f7087480052786835ebf
Svauser <svauser2170@gmail.com>
2024-03-31 04:46:13 +0600

    fourth commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from post.views import hello_view, current_date_view, goodbye_view, product_list_view, main_view, category_view,\
     product_detail_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', hello_view, name='hello'),
@@ -11,4 +14,4 @@ urlpatterns = [
     path('', main_view, name='main'),
     path('category/', category_view, name='category'),
     path('products/<int:product_id>', product_detail_view, name='product_detail')
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- post/admin.py
+++ post/admin.py
@@ -1,7 +1,30 @@
 from django.contrib import admin
 
 from post.models import Product, Review, Tag, Category
-admin.site.register(Product)
-admin.site.register(Review)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'created_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ['name']
+    readonly_fields = ('created_at', 'updated_at','id')
+    # fieldsets = (
+    #     (
+    #         None, {
+    #             'fields':('name','category','tags','price')
+    #         }),
+    #     (
+    #         'Readonly Fields',{
+    #             'fields':('id','created_at','updated_at'),
+    #             'classes':('collapse',)
+    #         }
+    #     )
+    #
+    # )
+    def save_model(self, request, obj, form, change):
+        obj.name = obj.name.capitalize()
+        super().save_model(request,obj,form,change)
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('product','text','created_at')
 admin.site.register(Tag)
 admin.site.register(Category)
